a5aefee1fa6f57c5ca851fb692b843b985feb86e
JoelSchecheleski <joel.jsp@gmail.com>
2018-11-07 23:07:22 -0200

    improvind category list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Category} from '../shared/category.module';
+import {CategoryService} from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  public categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {
+  }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente exluir esse registro?');
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element !== category),
+        () => alert('Erro ao tentar excluir!')
+      );
+    }
+
   }
 
 }
